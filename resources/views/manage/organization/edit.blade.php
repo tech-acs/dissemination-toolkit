9e604e565f597b82ad5c49c91f5e17fc1c36584a
Nahom <nahomt@amestsantim.com>
2026-06-19 11:41:54 +0300

    rename Organization page title to Settings

--- resources/views/manage/organization/edit.blade.php
+++ resources/views/manage/organization/edit.blade.php
@@ -2,10 +2,10 @@
 
     <x-slot name="header">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
-            {{ __('Organization') }}
+            {{ __('Settings') }}
         </h3>
         <p class="mt-2 max-w-7xl text-sm text-gray-500">
-            {{ __('Edit organization') }}
+            {{ __('Edit settings such as organization details, etc.') }}
         </p>
     </x-slot>
 
